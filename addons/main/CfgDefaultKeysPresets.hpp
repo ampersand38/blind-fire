944ba2a420a02249a6203b6d4dffee982075ed2b
Steve Zhao <ampersand38@gmail.com>
2022-01-07 12:51:27 -0500

    alt modifier for default keybinds

--- addons/main/CfgDefaultKeysPresets.hpp
+++ addons/main/CfgDefaultKeysPresets.hpp
@@ -2,8 +2,8 @@ class CfgDefaultKeysPresets {
 	class Arma2 { // Arma2 is inherited by all other presets.
 		class Mappings {
             GVAR(BlindFireToggle)[] = {"0x38130030"}; // Left Alt + B
-            GVAR(AimLeft)[] = {DIK_Q}; // Q
-            GVAR(AimRight)[] = {DIK_E}; // E
+            GVAR(AimLeft)[] = {"0x38130010"}; // Left Alt + Q
+            GVAR(AimRight)[] = {"0x38130012"}; // Left Alt + E
             /*
             GVAR(AimUp)[] = {"0x01320002"}; //
             GVAR(AimDown)[] = {"0x01320003"}; //
